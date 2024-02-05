b19ef224b5fff2c8dff91b3837aba427e22c1d58
meenaarun295 <meenaarun295@gmail.com>
2024-02-05 15:29:34 +0530

    changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -61,6 +61,12 @@ var swiper = new Swiper(".digi-logo-slider", {
     },
   },
 });
+// function change(id) {
+//   var element = document.getElementById(id);
+//   if (element.classList.contains("empty-value")) {
+//     element.classList.remove("empty-value");
+//   }
+// }
 function btnClick(e) {
   e.preventDefault();
   const visitPM = document.getElementById("visitors-pm").value;
@@ -68,21 +74,40 @@ function btnClick(e) {
   const aOV = document.getElementById("aov").value;
   console.log(visitPM, orderPM, aOV);
   console.log("arun");
-  var conversationsPerMonth = parseFloat(
-    (visitPM / (3333.33 * 100)).toFixed(3)
-  );
-  var potentialAdditionalSales = parseFloat(
-    (orderPM / (769.23 * 100)).toFixed(3)
-  );
-  var potentialAOV = parseFloat((aOV / (90.9 * 100)).toFixed(3));
-  var addUnlockSales = parseFloat((potentialAOV / (76.92 * 100)).toFixed(3));
-  var monthlyROI = parseFloat(((addUnlockSales - 15) / (15 * 100)).toFixed(3));
-  document.getElementById("con-pm").textContent = conversationsPerMonth;
-  document.getElementById("addSales").textContent = potentialAdditionalSales;
-  document.getElementById("potAV").textContent = potentialAOV;
-  document.getElementById("addSalesFromSpritle").textContent = addUnlockSales;
-  document.getElementById("subsCost").textContent = "15";
-  document.getElementById("mon-ROI").textContent = monthlyROI;
-  document.getElementById("ROI").classList.remove("d-none");
+
+  if (!visitPM || !orderPM || !aOV) {
+    // if (!visitPM.value) {
+    //   document.getElementById("visitors-pm").classList.add("empty-value");
+    // }
+
+    // if (!orderPM.value) {
+    //   document.getElementById("order-pm").classList.add("empty-value");
+    // }
+
+    // if (!aOV.value) {
+    //   document.getElementById("aov").classList.add("empty-value");
+    // }
+    console.log("error");
+  } else {
+    var conversationsPerMonth = parseFloat(
+      ((visitPM / 3333.33) * 100).toFixed(0)
+    );
+    var potentialAdditionalSales = parseFloat(
+      ((orderPM / 769.23) * 100).toFixed(0)
+    );
+    var potentialAOV = parseFloat(((aOV / 90.9) * 100).toFixed(0));
+    var addUnlockSales = parseFloat(((potentialAOV / 76.92) * 100).toFixed(0));
+    var monthlyROI = parseFloat(
+      (((addUnlockSales - 15) / 15) * 100).toFixed(0)
+    );
+    document.getElementById("con-pm").textContent = conversationsPerMonth;
+    document.getElementById("addSales").textContent = potentialAdditionalSales;
+    document.getElementById("potAV").textContent = "$" + potentialAOV;
+    document.getElementById("addSalesFromSpritle").textContent =
+      "$" + addUnlockSales;
+    document.getElementById("subsCost").textContent = "$15";
+    document.getElementById("mon-ROI").textContent = monthlyROI + "%";
+    document.getElementById("ROI").classList.remove("d-none");
+  }
 }
 document.getElementById("myButton").addEventListener("click", btnClick);
